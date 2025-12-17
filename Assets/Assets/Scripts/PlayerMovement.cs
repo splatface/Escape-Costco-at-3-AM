27117ad197b974cb4802bbcf47b37146b3d79988
Sonia O <sonicu.olar@gmail.com>
2025-12-17 13:07:42 -0500

    Updated playerMovement script

--- Assets/Assets/Scripts/PlayerMovement.cs
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float speed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
