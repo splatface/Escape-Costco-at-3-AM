79a3b6f85eee0e9e5f28d0935a5fddc0b35edfc5
Sonia O <sonicu.olar@gmail.com>
2026-01-05 18:15:37 -0500

    Prevented diagonal player movement and added player walk animations

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -1,26 +1,62 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed = 10f;
+    private Animator _anim;
+    private Vector3 _movementDirection;
 
-    void Update()
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Update()
     {
         Walk();
+        AnimatePlayer();
     }
 
     public void Walk()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
+        _movementDirection = Vector3.zero;
 
-        Vector3 xMovement = new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0);
-        transform.position += xMovement;
+        if (horizontal != 0)
+        {
+            _movementDirection = new Vector3(horizontal, 0f, 0f);
+        }
+        else if (vertical != 0)
+        {
+            _movementDirection = new Vector3(0f, vertical, 0f);
+        }
 
-        Vector3 movement = new Vector3(0.0f, verticalInput, 0.0f); 
-        transform.position += movement * moveSpeed * Time.deltaTime;    
+        transform.position += _movementDirection * moveSpeed * Time.deltaTime;
     }
-    
+
+    public void AnimatePlayer()
+    {
+        if (_movementDirection == Vector3.down)
+        {
+            _anim.SetBool("WalkDown", true);
+        } else if (_movementDirection == Vector3.up)
+        {
+            _anim.SetBool("WalkUp", true);
+        } else if (_movementDirection == Vector3.left)
+        {
+            _anim.SetBool("WalkLeft", true);
+        } else if (_movementDirection == Vector3.right)
+        {
+            _anim.SetBool("WalkRight", true);
+        } else
+        {
+            _anim.SetBool("WalkDown", false);
+            _anim.SetBool("WalkUp", false);
+            _anim.SetBool("WalkLeft", false);
+            _anim.SetBool("WalkRight", false);
+        }
+    }
 }
