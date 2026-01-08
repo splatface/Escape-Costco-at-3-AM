3bf9a0e983b69c2c0450d4ccef75089605910535
leanne <leannewong38@gmail.com>
2026-01-08 13:23:38 -0500

    added sound assets and audiomanager script

--- Assets/Assets/Scripts/Guard.cs
+++ Assets/Assets/Scripts/Guard.cs
@@ -2,24 +2,16 @@ using UnityEngine;
 
 public class AutoMoveBackAndForth : MonoBehaviour
 {
-
 public float delta = 3.0f;
 public float speed = 2.0f;
-
 private Vector3 startPos;
-
 void Start()
 {
-
 startPos = transform.position;
 }
-
 void Update()
 {
-
 float newX = startPos.x + delta * Mathf.Sin(Time.time * speed);
-
-
 transform.position = new Vector3(newX, startPos.y, startPos.z);
 }
 }
