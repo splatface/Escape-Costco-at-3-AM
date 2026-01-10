91434414cc088f03b9167b0cafe05c25d8352ba1
Tiffany Shao <tiffanyshao73@gmail.com>
2026-01-09 22:32:05 -0500

    started manager movement pattern; added recursion and made the movement based on distance from the player + added dodge based on player's attack (need to adjust still)

--- Assets/Assets/Scripts/ManagerEnemy.cs
+++ Assets/Assets/Scripts/ManagerEnemy.cs
@@ -1,25 +1,88 @@
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ManagerEnemy : MonoBehaviour
 {
-    private Vector3 _newPosition;
 
+    private int _attackValue = 10;
+    private int _healthValue = 70;
+
+    public PlayerMovement Player;
+
+    private float CalculateDistanceApart(Vector3 playerPos, Vector3 managerPos)
+    {
+
+        float x = playerPos.x - managerPos.x;
+        float y = playerPos.y - managerPos.y;
+
+        float distance = Mathf.Sqrt((float)(Mathf.Pow(x, 2) + Mathf.Pow(y, 2)));
+
+        return distance;
+    }
+
+    //movement the manager makes under normal conditions
     public void NormalMovement()
     {
-        for (int i=0; i<4; i += 1)
+        Vector3 playerPos = Player.GetCurrentPosition();
+        Vector3 managerPos = transform.position;
+
+        float distance = CalculateDistanceApart(playerPos, managerPos);
+
+        if (distance > 2f)
         {
-            transform.position += new Vector3(0, (float)0.1); // temporary just to test
+            if (playerPos.x < managerPos.x)
+            {
+                transform.position -= new Vector3 (0.1f, 0);
+            }
+            else if (playerPos.x > managerPos.x)
+            {
+                transform.position += new Vector3 (0.1f, 0);
+            }
+            if (playerPos.y < managerPos.y)
+            {
+                transform.position -= new Vector3 (0, 0.1f);
+            }
+            else if (playerPos.y > managerPos.y)
+            {
+                transform.position += new Vector3 (0, 0.1f);
+            }
+
+            NormalMovement(); //check logic if recursion works, not sure
+            
         }
+    }
+
+    // manager dodges; either set a time cooldown or some restrictions for activation (e.g. distance apart)
+    public void Dodge()
+    {
+        if (Keyboard.current.spaceKey.isPressed) //may need to change key; if player attacks, will move upwards to dodge
+        {
+            for (int i=0; i<4; i += 1)
+            {
+                transform.position = new Vector3 (0, 0.1f);
+            }
+        }
+    }
+
+    public void ChargeULT()
+    {
+        
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        NormalMovement(); // need to find a condition in update to call it on; check
     }
 
     // Update is called once per frame
     void Update()
     {
-        NormalMovement();
+    
+        if (_healthValue < 10)
+        {
+            ChargeULT();
+        }
     }
 }
