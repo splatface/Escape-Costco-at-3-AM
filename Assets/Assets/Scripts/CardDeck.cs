b94bd14d8dafd2ec3d22d97420550c9f6619928e
hselenal <selenaliao2008@gmail.com>
2026-01-14 13:06:30 -0500

    continued working on sorting algorithm for playing cards

--- Assets/Assets/Scripts/CardDeck.cs
+++ Assets/Assets/Scripts/CardDeck.cs
@@ -16,28 +16,20 @@ public class CardDeck : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject spawncard1 = Instantiate(Card1, transform.position, transform.rotation);
-        cards[0] = spawncard1;
-        GameObject spawncard2 = Instantiate(Card2, transform.position, transform.rotation);
-        cards[1] = spawncard2;
-        GameObject spawncard3 = Instantiate(Card3, transform.position, transform.rotation);
-        cards[2] = spawncard3;
-        GameObject spawncard4 = Instantiate(Card4, transform.position, transform.rotation);
-        cards[3] = spawncard4;
-        GameObject spawncard5 = Instantiate(Card5, transform.position, transform.rotation);
-        cards[4] = spawncard5;
-        GameObject spawncard6 = Instantiate(Card6, transform.position, transform.rotation);
-        cards[5] = spawncard6;
-        GameObject spawncard7 = Instantiate(Card7, transform.position, transform.rotation);
-        cards[6] = spawncard7;
-        GameObject spawncard8 = Instantiate(Card8, transform.position, transform.rotation);
-        cards[7] = spawncard8;
-        Shuffle();
-        foreach (GameObject card in cards)
-        {
-            Card cardobject = card.GetComponent<Card>();
-            Debug.Log(cardobject.GetNumber());
-        }
+        cards[0] = Card1;
+        cards[1] = Card2;
+        cards[2] = Card3;
+        cards[3] = Card4;
+        cards[4] = Card5;
+        cards[5] = Card6;
+        cards[6] = Card7;
+        cards[7] = Card8;
+        InstantiateAll();
+        //foreach (GameObject card in cards)
+        //{
+        //    Card cardobject = card.GetComponent<Card>();
+        //    Debug.Log(cardobject.GetNumber());
+        //}
 
     }
 
@@ -46,6 +38,20 @@ public class CardDeck : MonoBehaviour
     {
         
     }
+    public void InstantiateAll()
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            Instantiate(cards[i], new Vector3(-7f + i*2, 0f, 1f), transform.rotation);
+        }
+    }
+    public void RemoveAll()
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i].SetActive(false);
+        }
+    }
     public void Shuffle()
     {
         // sorting by numbers first, then by suits (alphabetical) within the numbers 
@@ -53,13 +59,44 @@ public class CardDeck : MonoBehaviour
         {
             Card card1 = cards[j].GetComponent<Card>();
             Card card2 = cards[j+1].GetComponent<Card>();
+            // sorting by number here 
             if (card1.GetNumber() > card2.GetNumber())
             {
                 GameObject toSwap = cards[j];
                 cards[j] = cards[j+1];
                 cards[j+1] = toSwap; 
             }
+            // sorting by suit here 
+            else if (card1.GetNumber() == card2.GetNumber())
+            {
+                // letters: c, d, h, s 
+                if (card2.GetSuit() == "club")
+                {
+                    GameObject toSwap = cards[j];
+                    cards[j] = cards[j + 1];
+                    cards[j + 1] = toSwap;
+                }
+                else if (card2.GetSuit() == "diamond")
+                {
+                    if (card1.GetSuit() != "club")
+                    {
+                        GameObject toSwap = cards[j];
+                        cards[j] = cards[j + 1];
+                        cards[j + 1] = toSwap;
+                    }
+                }
+                else if (card2.GetSuit() == "heart")
+                {
+                    if (card1.GetSuit() == "spade")
+                    {
+                        GameObject toSwap = cards[j];
+                        cards[j] = cards[j + 1];
+                        cards[j + 1] = toSwap;
+                    }
+                }
+            }
         }
         
     }
+
 }
