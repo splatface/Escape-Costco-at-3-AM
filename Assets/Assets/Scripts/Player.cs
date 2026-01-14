04ae6bbcc4f099160769b87ddd32d8fe1052c058
Sonia O <sonicu.olar@gmail.com>
2026-01-14 13:15:45 -0500

    Created win screen. Added comments to player class. Added sort buttons to shop and created shop manager class.

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -4,11 +4,13 @@ using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     private float _moveSpeed = 10f;
     private int _health = 100;
     private Animator _anim;
     private Vector3 _movementDirection;
     private ItemBaseClass _weapon;
+    //Aggregaton where the Player holds and uses the ItemBaseClass class
 
     public void Awake()
     {
@@ -24,13 +26,17 @@ public class PlayerMovement : MonoBehaviour
 
     public void Walk()
     {
+        //Get arrow key input
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        //Set direction to nothing first
         this.SetMovementDirection(Vector3.zero);
 
+        //Prevent the character from moving diagonally -- restrict to up, down, left, right so that it can be animated
         if (horizontal != 0)
         {
+            //Identify and save direction so that player can be animated accordingly
             this.SetMovementDirection(new Vector3(horizontal, 0f, 0f));
         }
         else if (vertical != 0)
@@ -38,12 +44,8 @@ public class PlayerMovement : MonoBehaviour
             this.SetMovementDirection(new Vector3(0f, vertical, 0f));
         }
 
+        //Move player
         transform.position += this.GetMovementDirection() * this.GetMoveSpeed() * Time.deltaTime;
-    }
-
-    public Vector3 GetCurrentPosition()
-    {
-        return transform.position;
     }
 
     public void AnimatePlayer()
@@ -51,15 +53,22 @@ public class PlayerMovement : MonoBehaviour
         Animator anim = GetAnimator();
         Vector3 direction = GetMovementDirection();
 
+        //Use direction to animate player accordingly
         anim.SetBool("WalkDown",  direction == Vector3.down);
         anim.SetBool("WalkUp",    direction == Vector3.up);
         anim.SetBool("WalkLeft",  direction == Vector3.left);
         anim.SetBool("WalkRight", direction == Vector3.right);
     }
 
+    public Vector3 GetCurrentPosition()
+    {
+        return transform.position;
+    }
+
     public float GetMoveSpeed()
     {
-        if (_moveSpeed <= 0)
+        //If the move speed is somehow set out of range, it should not be used
+        if (_moveSpeed <= 0 || _moveSpeed > 10)
         {
             throw new System.Exception("Move speed is negative.");
         }
@@ -69,6 +78,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetMoveSpeed(float newSpeed)
     {
+        //Move speed can only be set between 0 and 10
         if (newSpeed > 0 && newSpeed < 10)
         {
             this._moveSpeed = newSpeed;
@@ -80,6 +90,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Animator GetAnimator()
     {
+        //Make sure there is an animator before further using it
         if (_anim == null)
         {
             throw new System.Exception("Animator is null.");
@@ -109,6 +120,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetHealth(int newHealth)
     {
+        //Make sure health cannot be set over maximum or under minimum
         if (newHealth < 0)
         {
             this._health = 0;
---
+++ Assets/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public List<PowerUpEffect> _powerups = new List<PowerUpEffect>();
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
