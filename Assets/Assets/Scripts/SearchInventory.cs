d3a2404752c9730558bebf243604f4d469b319a4
Tiffany Shao <tiffanyshao73@gmail.com>
2026-01-20 10:37:00 -0500

    finished searching through titles for inventory

--- Assets/Assets/Scripts/SearchInventory.cs
+++ Assets/Assets/Scripts/SearchInventory.cs
@@ -1,14 +1,33 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class SearchInventory : MonoBehaviour
+public class SearchInventoryTitle : MonoBehaviour
 {
     public TMP_InputField SearchInputField;
 
     public void GetEnteredText()
     {
-        // string text = SearchInputField.text;
+        string text = SearchInputField.text;
 
-        // FullInventory.Instance.ShowInvenItems(text);
+        string[] allInvenItems = FullInventory.Instance.GetAllItems();
+        List<string> matchingItems = new List<string>();
+
+        foreach (string item in allInvenItems)
+        {
+
+            if (!string.IsNullOrWhiteSpace(item))
+            {
+                text = text.Trim();
+                string itemShort = item.Trim();
+                if (itemShort.Contains(text, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    matchingItems.Add(item);
+                }
+            }
+        }
+
+        FullInventory.Instance.DestroyInvenItems();
+        FullInventory.Instance.ShowInvenItems(matchingItems.ToArray());
     }
 }
