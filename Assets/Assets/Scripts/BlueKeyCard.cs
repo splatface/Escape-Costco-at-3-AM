2f554234509c9515ad9ff5759df59c29e13b7dd4
hselenal <selenaliao2008@gmail.com>
2026-01-21 13:14:20 -0500

    continued working on keycards and doors

--- Assets/Assets/Scripts/BlueKeyCard.cs
+++ Assets/Assets/Scripts/BlueKeyCard.cs
@@ -9,11 +9,6 @@ public class BlueKeyCard : KeyCard
     }
     protected override void Update()
     {
-        OpenDoor();
-    }
-    public void OpenDoor()
-    {
-        // check if player equipped blue keycard and is touching the blue room door 
-        // if so, open blue room scene 
+        
     }
 }
---
+++ Assets/Assets/Scripts/RedKeyCardCollector.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RedKeyCardCollector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            FullInventory.Instance.PlaceIntoInven("RedKeyCard");
+            Destroy(this.gameObject);
+        }
+    }
+}
