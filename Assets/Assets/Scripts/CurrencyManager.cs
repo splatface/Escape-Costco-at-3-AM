ddac76a47554811cae8c162d4e30daea21df6161
hselenal <selenaliao2008@gmail.com>
2026-01-21 20:50:25 -0500

    debugging currency system and purchasing from shop

--- Assets/Assets/Scripts/CurrencyManager.cs
+++ Assets/Assets/Scripts/CurrencyManager.cs
@@ -1,27 +1,37 @@
 using UnityEngine;
+using TMPro;
 
 public class CurrencyManager : MonoBehaviour
 {
     private int _coins;
 
+    public TextMeshProUGUI CoinCount;
+
+    public static CurrencyManager Instance { get; set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetCoinCount();
+    }
+
+    void Awake()
+    {
+        if (Instance == null) // makes it so that only one exists at a time
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // destroy(this) would destroy the one we want
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Coin"))
-        {
-            this._coins += 1;
-            Destroy(other.gameObject);
-        }
     }
     public int GetCoins()
     {
@@ -30,9 +40,15 @@ public class CurrencyManager : MonoBehaviour
     public void AddCoins(int amount)
     {
         this._coins += amount;
+        SetCoinCount();
     }
     public void SubtractCoins(int amount)
     {
         this._coins -= amount;
+        SetCoinCount();
+    }
+    public void SetCoinCount()
+    {
+        CoinCount.text = "Coins:" + this._coins.ToString();
     }
 }
