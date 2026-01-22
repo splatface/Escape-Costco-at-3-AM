b035594e3684816886493cf4bd1f034ddc113d50
Sonia O <sonicu.olar@gmail.com>
2026-01-21 20:13:16 -0500

    Fixed player class name.

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     private float _moveSpeed = 10f;
